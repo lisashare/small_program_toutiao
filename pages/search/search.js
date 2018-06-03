fbdcaf4eab8fde0ecf1f0047cd9dc68550b11bbd
lisashare <lisa_zhu2012@foxmail.com>
2018-06-03 23:34:19 +0800

    搜索页面使用了weui，模拟首页的数据进行的搜索

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,23 +1,34 @@
 // pages/search/search.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+Page({
   data: {
-
+    lists: [],
+    keyword: ''
   },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  formSubmit (e) {
+    let keyword = e.detail.value
+    this.setData({keyword, lists:[]})
+    this.getLists()
+  },
+  getLists () {
+    wx.request({
+      url: 'https://m.toutiao.com/list/?ac=wap&count=20&format=json_raw&as=A1A53AFF9ABB2CD&cp=5AFA9BA2EC2DEE1&_signature=XTrrwwAAB8AKDaOY8NEXVF0669&i=', //仅为示例，并非真实的接口地址
+      data: {
+         tag: '__all__',
+         min_behot_time: Math.floor(Date.now()/1000),
+         keyword:this.data.keyword
+      },
+      header: {
+          'content-type': 'application/json' // 默认值
+      },
+      success: (res) => {
+        var lists = []
+        lists = this.data.lists.concat(res.data.data)     
+        this.setData({lists})
+        wx.stopPullDownRefresh()
+      }
+    })
+  },
+  onReachBottom () {
+    this.getLists()
   }
 })
