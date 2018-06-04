4cf786478f67dd1899e408cac8e342faf1cede44
lisashare <lisa_zhu2012@foxmail.com>
2018-06-04 10:08:31 +0800

    登录，注销功能实现

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,4 +1,7 @@
 // pages/mine/mine.js
+
+let app = getApp()
+
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +14,26 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    userInfo : null
   },
-
+  onload () {
+    // this.setData({ userInfo:app.globalData.userInfo });
+  },
   /**
    * 组件的方法列表
    */
   methods: {
+    getUserInfo (e) {
+      // console.log(e);
+      let userInfo = e.detail.userInfo;
+      app.globalData.userInfo = userInfo;
+      this.setData({ userInfo });
+      wx.setStorageSync("userInfo",userInfo);  //存储本地
+    },
+    exit () {
+      this.setData({ userInfo:null });
+      wx.removeStorageSync("userInfo");
+    }
 
   }
 })
